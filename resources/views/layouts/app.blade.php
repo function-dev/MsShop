8db37af4f3872dc91288bad2ad74a6be3374bd0b
Egor <edrozdovskiy@gmail.com>
2021-02-22 14:21:09 +0300

    стили

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,13 +5,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="stylesheet" href="{{ asset('css/site.css') }}">
+
+    <title>MultiSport Shop</title>
 </head>
 <body>
 
+<div class="container">
+    <header class="header">
+        <h1 class="header-title">MsShop</h1>
+        <ul class="header__nav">
+            <li class="header__nav-item">
+                <a href="#" class="header__nav-link">Каталог</a>
+            </li>
+            <li class="header__nav-item">
+                <a href="#" class="header__nav-link">Корзина</a>
+            </li>
+            <li class="header__nav-item">
+                <a href="#" class="header__nav-link">Информация</a>
+            </li>
+            <li class="header__nav-item">
+                <a href="#" class="header__nav-link">Таблица размеров</a>
+            </li>
+        </ul>
+    </header>
+</div>
 
 @yield('content')
-
 
 </body>
 </html>
